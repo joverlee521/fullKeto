4bf6fe4248ba5ecb2478bc24ef2d550b29a94eae
J.Lee <joverlee521@gmail.com>
2018-12-10 12:21:23 -0800

    added handlebars to the application

--- models/user.js
+++ models/user.js
@@ -2,7 +2,10 @@ module.exports = function(sequelize, DataTypes){
     var User = sequelize.define("User", {
         username: {
             type: DataTypes.STRING,
-            allowNull: false
+            allowNull: false,
+            validate: {
+                len: [2, 50]
+            }
         },
         token: {
             type: DataTypes.TEXT,
--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 // Outer dependencies.
+require("dotenv").config();
 var express = require("express");
 var passport = require("passport");
 var auth = require("./config/auth");
 var cookieParser = require("cookie-parser");
 var cookieSession = require("cookie-session");
+var exphbs = require("express-handlebars");
+
+// Requires our models for syncing.
+var db = require("./models");
 
 // Sets up the express app.
 var app = express();
@@ -13,31 +18,28 @@ var PORT = process.env.PORT || 8080;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-
-
+// Set up express handlebars
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
 
 // Static directory
 app.use(express.static("public"));
 
-// Routes
-require("dotenv").config();
-
+// Passport
 auth(passport);
 app.use(passport.initialize());
 
+// Cookie Session
 app.use(cookieSession({
     name: "session", 
     keys: [process.env.SESSION_KEY]
 }));
 app.use(cookieParser());
 
+// Routes
 require("./routes/apiRoutes")(app);
 require("./routes/authRoutes")(app);
-
-
-
-// Requires our models for syncing.
-var db = require("./models");
+require("./routes/htmlRoutes")(app);
 
 // Syncing our models and starting app.
 db.sequelize.sync({force: true}).then(function(){
