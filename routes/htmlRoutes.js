accbef0a500c7038c6515a75e54feb66e7e8be68
J.Lee <joverlee521@gmail.com>
2018-12-11 10:12:09 -0800

    changed the color scheme of buttons to dark orange

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -3,4 +3,8 @@ module.exports = function(app){
     app.get("/", function(req, res){
         res.render("index");
     });
+    
+    app.get("/login", function(req, res){
+        res.render("logInPage");
+    })
 };
